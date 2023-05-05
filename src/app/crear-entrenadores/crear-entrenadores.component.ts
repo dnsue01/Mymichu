8852d1e1228c9adb085cd14bb5c4e3fd2d7de486
daniel nsue <danielnsuemartinez@gmail.com>
2023-05-05 12:55:35 +0200

    crear Entrenador Visual

--- src/app/crear-entrenadores/crear-entrenadores.component.ts
+++ src/app/crear-entrenadores/crear-entrenadores.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-crear-entrenadores',
   templateUrl: './crear-entrenadores.component.html',
-  styleUrls: ['./crear-entrenadores.component.scss']
+  styleUrls: ['./crear-entrenadores.component.scss'],
 })
 export class CrearEntrenadoresComponent {
-
+  usuario = {
+    correo: '',
+    nombre: '',
+    contrasenna: '',
+  };
 }
